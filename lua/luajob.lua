e46772e65593609e4674144ef7d4639f77ab3e14
TravonteD <tman1300@aol.com>
2020-02-15 22:36:50 -0500

    move options setting to a function

--- lua/luajob.lua
+++ lua/luajob.lua
@@ -26,25 +26,40 @@ function M:shutdown(code, signal)
   M.handle:close()
 end
 
-function M:start()
+function M:options()
   local options = {}
   local args  = vim.fn.split(M.cmd, ' ')
+
   M.stdin = vim.loop.new_pipe(false)
   M.stdout = vim.loop.new_pipe(false)
   M.stderr = vim.loop.new_pipe(false)
-  command = table.remove(args, 1)
+
+  options.command = table.remove(args, 1)
   options.args = args
-  options.stdio = { M.stdin, M.stdout, M.stderr }
+  options.stdio = { 
+    M.stdin, 
+    M.stdout, 
+    M.stderr 
+  }
+
   if M.cwd then
     options.cwd = M.cwd
   end
+
   if M.env then
     options.env = M.cwd
   end
+
   if M.detach then
     options.detach = M.detach
   end
-  M.handle = vim.loop.spawn(command, 
+
+  return options
+end
+
+function M:start()
+  options = M.options()
+  M.handle = vim.loop.spawn(options.command, 
     options, 
     vim.schedule_wrap(M.shutdown))
   if M.on_stdout then
